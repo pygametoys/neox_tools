bbfeea6604a8367fb8da3635f620f149992b14dc
SoulMelody <SoulMelody@users.noreply.github.com>
2019-11-05 14:59:15 +0800

    support lz4 decompression

--- nxpk_extractor.py
+++ nxpk_extractor.py
@@ -1,5 +1,83 @@
-import os, struct, zlib, tempfile, argparse
+import os, struct, zlib, io, argparse, csv, ntpath
 from tqdm import tqdm
+from six import byte2int
+
+
+class CorruptError(Exception):
+    pass
+
+
+def lz4_uncompress(src):
+    """uncompress a block of lz4 data.
+    :param bytes src: lz4 compressed data (LZ4 Blocks)
+    :returns: uncompressed data
+    :rtype: bytearray
+    .. seealso:: http://cyan4973.github.io/lz4/lz4_Block_format.html
+    """
+    src = io.BytesIO(src)
+
+    # if we have the original size, we could pre-allocate the buffer with
+    # bytearray(original_size), but then we would have to use indexing
+    # instad of .append() and .extend()
+    dst = bytearray()
+    min_match_len = 4
+
+    def get_length(src, length):
+        """get the length of a lz4 variable length integer."""
+        if length != 0x0f:
+            return length
+
+        while True:
+            read_buf = src.read(1)
+            if len(read_buf) != 1:
+                raise CorruptError("EOF at length read")
+            len_part = byte2int(read_buf)
+
+            length += len_part
+
+            if len_part != 0xff:
+                break
+
+        return length
+
+    while True:
+        # decode a block
+        read_buf = src.read(1)
+        if len(read_buf) == 0:
+            raise CorruptError("EOF at reading literal-len")
+        token = byte2int(read_buf)
+
+        literal_len = get_length(src, (token >> 4) & 0x0f)
+
+        # copy the literal to the output buffer
+        read_buf = src.read(literal_len)
+
+        if len(read_buf) != literal_len:
+            raise CorruptError("not literal data")
+        dst.extend(read_buf)
+
+        read_buf = src.read(2)
+        if len(read_buf) == 0:
+            if token & 0x0f != 0:
+                raise CorruptError("EOF, but match-len > 0: %u" % (token % 0x0f, ))
+            break
+
+        if len(read_buf) != 2:
+            raise CorruptError("premature EOF")
+
+        offset = byte2int([read_buf[0]]) | (byte2int([read_buf[1]]) << 8)
+
+        if offset == 0:
+            raise CorruptError("offset can't be 0")
+
+        match_len = get_length(src, (token >> 0) & 0x0f)
+        match_len += min_match_len
+
+        # append the sliding window of the previous literals
+        for _ in range(match_len):
+            dst.append(dst[-offset])
+
+    return dst
 
 
 def readuint32(f):
@@ -60,14 +138,14 @@ def get_ext(data):
 
 def unpack(opt):
     folder_path = opt.path.replace('.npk', '')
-    os.mkdir(folder_path)
-
+    map_path = opt.path + '.map'
 
     with open(opt.path, 'rb') as f:
+        index_table = []
+        index_map = {}
         data = f.read(4)
         assert data == b'NXPK'
         files = readuint32(f)
-        print(files)
         var1 = readuint32(f)
         var2 = readuint32(f)
         var3 = readuint32(f)
@@ -75,34 +153,60 @@ def unpack(opt):
         info_size = 0x28 if mode else 0x1c
         index_offset = readuint32(f)
         f.seek(index_offset)
-        index_table = []
-        with tempfile.TemporaryFile() as tmp:
-            for i in range(files * 28):
-                data = readuint8(f)
-                tmp.write(struct.pack('B', data))
-            tmp.seek(0)
-            for _ in range(files):
-                file_sign = readuint32(tmp)
-                file_offset = readuint32(tmp)
-                file_length = readuint32(tmp)
-                file_original_length = readuint32(tmp)
-                file_hash_1 = readuint32(tmp)
-                file_hash_2 = readuint32(tmp)
-                file_flag = readuint32(tmp)
-                index_table.append((
-                    file_offset, 
-                    file_length,
-                    file_original_length, 
-                    file_flag,
-                    ))
+        with io.BytesIO() as tmp:
+            if os.path.exists(map_path):
+                with open(map_path) as fm:
+                    reader = csv.DictReader(fm, ['path', 'sign', 'length', 'hash1', 'ori_length', 'hash2', 'offset'], delimiter='\t')
+                    for row in reader:
+                        index_map[int(row['sign'])] = row['path']
+
+                    for i in range(files * 28):
+                        data = readuint8(f)
+                        tmp.write(struct.pack('B', data))
+                    tmp.seek(0)
+                    for _ in range(files):
+                        file_sign = readuint32(tmp)
+                        file_offset = readuint32(tmp)
+                        file_length = readuint32(tmp)
+                        file_original_length = readuint32(tmp)
+                        file_hash_1 = readuint32(tmp)
+                        file_hash_2 = readuint32(tmp)
+                        file_flag = readuint32(tmp)
+                        index_table.append((
+                            file_offset,
+                            file_length,
+                            file_original_length,
+                            file_flag,
+                            index_map[file_sign]
+                            ))
+            else:
+                for i in range(files * 28):
+                    data = readuint8(f)
+                    tmp.write(struct.pack('B', data))
+                tmp.seek(0)
+                for _ in range(files):
+                    file_sign = readuint32(tmp)
+                    file_offset = readuint32(tmp)
+                    file_length = readuint32(tmp)
+                    file_original_length = readuint32(tmp)
+                    file_hash_1 = readuint32(tmp)
+                    file_hash_2 = readuint32(tmp)
+                    file_flag = readuint32(tmp)
+                    index_table.append((
+                        file_offset,
+                        file_length,
+                        file_original_length,
+                        file_flag,
+                        ))
 
         for i, item in tqdm(enumerate(index_table)):
-            file_name = '{:8}.dat'.format(i)
-            file_offset, file_length, file_original_length, file_flag = item
-            if file_length < 5000:
-                continue
+            if len(item) > 4:
+                file_offset, file_length, file_original_length, file_flag, file_name = item
+            else:
+                file_name = '{:8}.dat'.format(i)
+                file_offset, file_length, file_original_length, file_flag = item
             f.seek(file_offset)
-            with tempfile.TemporaryFile() as tmp:
+            with io.BytesIO() as tmp:
                 for i in range(file_length):
                     data = readuint8(f)
                     tmp.write(struct.pack('B', data))
@@ -111,11 +215,21 @@ def unpack(opt):
 
             if file_flag == 1:
                 data = zlib.decompress(data)
-            ext = get_ext(data)
-            file_name = '{:08}.{}'.format(i, ext)
-            if ext in ['nxm', 'ktx']:
-                with open(folder_path + '/' + file_name , 'wb') as dat:
-                    dat.write(data)
+            elif file_flag == 2:
+                data = lz4_uncompress(data)
+            if len(item) == 4:
+                ext = get_ext(data)
+                file_name = '{:08}.{}'.format(i, ext)
+                folder_path_real = folder_path
+            else:
+                file_name = file_name.replace(ntpath.sep, '/')
+                folder_path_real = folder_path + '/' + os.path.dirname(file_name)
+                file_name = os.path.basename(file_name)
+                _, ext = os.path.splitext(file_name)
+            os.makedirs(folder_path_real, exist_ok=True)
+            with open(folder_path_real + '/' + file_name , 'wb') as dat:
+                dat.write(data)
+
 def get_parser():
     parser = argparse.ArgumentParser(description='NXPK Extractor')
     parser.add_argument('path', type=str)
